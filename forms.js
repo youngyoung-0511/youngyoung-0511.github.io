3d6ce70669189da932194e297239581874da63d8
youngyoung-0511 <st0918710376@gmail.com>
2024-06-11 19:10:30 +0800

    Add files via upload

---
+++ forms.js
@@ -0,0 +1,19 @@
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // 防止表單提交
+
+    // 簡單的表單驗證
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (name && email && message) {
+        // 顯示感謝信息
+        document.getElementById('thankYouMessage').style.display = 'block';
+
+        // 清空表單
+        document.getElementById('contactForm').reset();
+    } else {
+        alert('請填寫所有欄位。');
+    }
+});
---
+++ sp.js
@@ -0,0 +1,7 @@
+// function changePage(pageId) { // 定義一個名為 changePage 的函數，接受一個名為 pageId 的參數
+//     var currentPage = document.querySelector('.current'); // 獲取當前顯示的頁面
+//     var nextPage = document.getElementById(pageId); // 獲取要顯示的下一個頁面
+    
+//     currentPage.classList.remove('current'); // 從當前頁面中移除 'current' 類
+//     nextPage.classList.add('current'); // 將 'current' 類添加到下一個頁面，使其顯示
+// }
